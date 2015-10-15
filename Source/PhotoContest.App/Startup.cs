c1d95c563a6fff761fe887150f69287406aab89e
BarishYumerov <barish.yumerov@gmail.com>
2015-10-15 21:01:54 +0300

    Revert "Revert "Added .App project sceleton""
    This reverts commit da909081e94743a8029ba875c4546e041d441432.

---
+++ Source/PhotoContest.App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhotoContest.App.Startup))]
+namespace PhotoContest.App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
