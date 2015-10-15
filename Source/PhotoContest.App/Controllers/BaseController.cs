3e358e617770deab43622edaae59c043a186ad64
BarishYumerov <barish.yumerov@gmail.com>
2015-10-15 21:17:18 +0300

    Added base controller template and references!

---
+++ Source/PhotoContest.App/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿namespace PhotoContest.App.Controllers
+{
+    using System.Web.Mvc;
+    //using PhotoContest.Data.UnitOfWork;
+
+    public class BaseController : Controller
+    {
+        //private IPhotoContestData data;
+
+        //public BaseController(IPhotoContestData data)
+        //{
+        //    this.data = data;
+        //}
+
+        //protected IPhotoContestData Data
+        //{
+        //    get
+        //    {
+        //        return this.data;
+        //    }
+
+        //}
+    }
+}
