4aca8a3165f5931bbe3e408a6a65c813438b33d6
BarishYumerov <barish.yumerov@gmail.com>
2015-10-19 19:46:25 +0300

    added mock repos and mock database

---
+++ Source/PhotoContest.Tests/Mocks/GenericRepositoryMock.cs
@@ -0,0 +1,85 @@
+﻿namespace PhotoContest.Tests.Mocks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using PhotoContest.Data.Contracts;
+
+    public class GenericRepositoryMock<T> : IRepository<T> 
+        where T : class
+    {
+        private List<T> entities = new List<T>();
+
+        private Func<T, object> keySelector; 
+
+        public bool ChangesSaved { get; set; }
+
+        public GenericRepositoryMock(Func<T, object> keySelector = null)
+        {
+            if (keySelector != null)
+            {
+                this.keySelector = keySelector;
+            }
+            else
+            {
+                this.keySelector = (u => ((dynamic)u).Id);
+            }
+        }
+
+        public IQueryable<T> All()
+        {
+            return this.entities.AsQueryable();
+        }
+
+        public T Find(object id)
+        {
+            return this.entities.FirstOrDefault(entity => id.Equals(this.keySelector(entity)));
+        }
+
+        public void Add(T entity)
+        {
+            this.entities.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            var existingEntity = this.Find(this.keySelector(entity));
+            var existingEntityIndex = this.entities.IndexOf(existingEntity);
+            this.entities[existingEntityIndex] = entity;
+        }
+
+        void IRepository<T>.Delete(T entity)
+        {
+            this.entities.Remove(entity);
+        }
+
+        public int SaveChanges()
+        {
+            this.ChangesSaved = true;
+            return 1;
+        }
+
+
+        public T Delete(T entity)
+        {
+            var existingEntity = this.Find(this.keySelector(entity));
+            var existingEntityIndex = this.entities.IndexOf(existingEntity);
+            this.entities.RemoveAt(existingEntityIndex);
+            return existingEntity;
+        }
+
+
+        public T Delete(object Id)
+        {
+            var existingEntity = this.Find(Id);
+            var existingEntityIndex = this.entities.IndexOf(existingEntity);
+            this.entities.RemoveAt(existingEntityIndex);
+            return existingEntity;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/PhotoContest.Tests/Mocks/PhotoContestDataMock.cs
@@ -0,0 +1,53 @@
+﻿namespace PhotoContest.Tests.Mocks
+{
+    using PhotoContest.Models;
+    using Data.Contracts;
+
+    public class PhotoContestDataMock : IPhotoContestData
+    {
+        private IRepository<User> usersRepositoryMock = new GenericRepositoryMock<User>();
+        private IRepository<Contest> contestsRepositoryMock = new GenericRepositoryMock<Contest>();
+        private IRepository<Picture> pictureRepositoryMock = new GenericRepositoryMock<Picture>();
+        private IRepository<Prize> prizesRepositoryMock = new GenericRepositoryMock<Prize>();
+        private IRepository<Vote> votesRepositoryMock = new GenericRepositoryMock<Vote>();
+        private IRepository<VotingCommittee> votingCommitteesRepositoryMock = new GenericRepositoryMock<VotingCommittee>();
+
+        public bool ChangesSaved { get; set; }
+
+        public IRepository<User> Users
+        {
+            get { return this.usersRepositoryMock; }
+        }
+
+        public IRepository<Contest> Contests
+        {
+            get { return this.contestsRepositoryMock; }
+        }
+
+        public IRepository<Picture> Pictures
+        {
+            get { return this.pictureRepositoryMock; }
+        }
+
+        public IRepository<Prize> Prizes
+        {
+            get { return this.prizesRepositoryMock; }
+        }
+
+        public IRepository<Vote> Votes
+        {
+            get { return this.votesRepositoryMock; }
+        }
+
+        public IRepository<VotingCommittee> Committees
+        {
+            get { return this.votingCommitteesRepositoryMock; }
+        }
+
+        public int SaveChanges()
+        {
+            this.ChangesSaved = true;
+            return 1;
+        }
+    }
+}
