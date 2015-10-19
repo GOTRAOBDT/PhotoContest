8ee6079133ca9b9ffd6ebaa20e1e94c132474c50
tockata <anatoly.angelov@gmail.com>
2015-10-19 11:23:39 +0300

    Changed .Net Framework to version 4.5.2 Added Test project

---
+++ Source/PhotoContest.Tests/UnitTests/MockContainer.cs
@@ -0,0 +1,11 @@
+﻿namespace PhotoContest.Tests.UnitTests
+{
+    using Data.Contracts;
+    using Models;
+    using Moq;
+
+    public class MockContainer
+    {
+        public Mock<IRepository<User>> UsersRepositoryMock { get; set; }
+    }
+}
