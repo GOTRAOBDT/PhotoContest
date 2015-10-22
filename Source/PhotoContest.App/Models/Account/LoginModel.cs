c9c0a55ae292b9d460a0901f864a475c52b7d4c9
straho99 <strahil.ruychev@gmail.com>
2015-10-22 15:01:22 +0300

    Created LoginModel closes #5

---
+++ Source/PhotoContest.App/Models/Account/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿namespace PhotoContest.App.Models.Account
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
