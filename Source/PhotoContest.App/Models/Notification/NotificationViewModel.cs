e6a3ca8ff31b405fb3c74f00b6a21cff99b34cea
straho99 <strahil.ruychev@gmail.com>
2015-10-22 09:12:19 +0300

    Created NotificationViewModel closes #55

---
+++ Source/PhotoContest.App/Models/Notification/NotificationViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PhotoContest.App.Models.Notification
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class NotificationViewModel
+    {
+        [Required]
+        public string RecipientUsername { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public bool IsRead { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+    }
+}
