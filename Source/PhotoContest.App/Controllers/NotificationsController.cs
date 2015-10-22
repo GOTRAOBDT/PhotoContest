7ede2076543e66c650d64150c9948e7ebd62e4c6
straho99 <strahil.ruychev@gmail.com>
2015-10-22 09:22:42 +0300

    Added Notifications Controller closes #54

---
+++ Source/PhotoContest.App/Controllers/NotificationsController.cs
@@ -0,0 +1,23 @@
+﻿namespace PhotoContest.App.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using Data.Contracts;
+
+    public class NotificationsController : BaseController
+    {
+        public NotificationsController(IPhotoContestData data)
+            : base(data)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
