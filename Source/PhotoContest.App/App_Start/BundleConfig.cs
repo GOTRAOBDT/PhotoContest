5800a42641e7bbf85b889dbe6027d642007fb845
tockata <anatoly.angelov@gmail.com>
2015-10-30 17:54:28 +0200

    1. Added Administration Area; 2. Added Manage controller to admin area; 3. Added Contests view for Manage/Contests action.

--- Source/PhotoContest.App/App_Start/BundleConfig.cs
+++ Source/PhotoContest.App/App_Start/BundleConfig.cs
@@ -26,6 +26,10 @@ namespace PhotoContest.App
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/ContentAdmin/css").Include(
+                      "~/Content/_bootstrap.css",
+                      "~/Content/site.css"));
         }
     }
 }
