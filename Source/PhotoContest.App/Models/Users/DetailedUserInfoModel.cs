3fa80b22959afb82044d83e982ec053191a5b1fe
straho99 <strahil.ruychev@gmail.com>
2015-10-31 16:06:13 +0200

    Implemented Users Controller

---
+++ Source/PhotoContest.App/Models/Users/DetailedUserInfoModel.cs
@@ -0,0 +1,30 @@
+﻿namespace PhotoContest.App.Models.Users
+{
+    using System;
+    using AutoMapper;
+    using Bookmarks.Common.Mappings;
+
+    using PhotoContest.Models;
+    using PhotoContest.Models.Enumerations;
+
+    public class DetailedUserInfoModel : IMapFrom<User>, IHaveCustomMappings
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public UserGender Gender { get; set; }
+
+        public string ProfilePicture { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+        }
+    }
+}
