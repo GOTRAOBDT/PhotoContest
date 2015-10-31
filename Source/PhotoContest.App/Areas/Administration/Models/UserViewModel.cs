cae1ef8d9f36b977d81c5271f93ff1712445d19c
tockata <anatoly.angelov@gmail.com>
2015-10-31 13:25:22 +0200

    Administration - implemented edit profile for users except admin profile.

--- Source/PhotoContest.App/Areas/Administration/Models/UserViewModel.cs
+++ Source/PhotoContest.App/Areas/Administration/Models/UserViewModel.cs
@@ -9,6 +9,8 @@
 
     public class UserViewModel : IMapFrom<User>
     {
+        public string Id { get; set; }
+
         public string UserName { get; set; }
 
         public string Name { get; set; }
