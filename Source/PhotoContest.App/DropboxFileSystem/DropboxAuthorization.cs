31ec3c60730f415f088a9ff623a47d6c14cc9527
asyamincheva <asya.m.mincheva@gmail.com>
2015-11-02 13:06:38 +0200

    Dropbox.Api methods

---
+++ Source/PhotoContest.App/DropboxFileSystem/DropboxAuthorization.cs
@@ -0,0 +1,28 @@
+﻿namespace PhotoContest.App.Dropbox
+{
+    using System;
+    using System.Threading.Tasks;
+    using Dropbox.Api;
+
+    public class DropboxAuthorization
+    {
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Run((Func<Task>)Program.Run);
+            task.Wait();
+        }
+
+        static async Task Run()
+        {
+            using (var dbx = new DropboxClient("YOUR ACCESS TOKEN"))
+            {
+                var full = await dbx.Users.GetCurrentAccountAsync();
+                Console.WriteLine("{0} - {1}", full.Name.DisplayName, full.Email);
+            }
+        }
+    }
+}
