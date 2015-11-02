424eaf5e16616dfef90ac01f0ffcae2c81cf25d3
asyamincheva <asya.m.mincheva@gmail.com>
2015-11-02 13:48:43 +0200

    fixed bugs in references

---
+++ Source/PhotoContest.App/DropboxFileSystem/DropboxFileSystem.cs
@@ -0,0 +1,63 @@
+﻿namespace PhotoContest.App.DropboxFileSystem
+{
+    using System;
+    using System.Threading.Tasks;
+    using Dropbox.Api;
+    using System.Linq;
+    using System.IO;
+    using System.Text;
+
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    var task = Task.Run((Func<Task>)Program.Run);
+        //    task.Wait();
+        //}
+
+        static async Task Run()
+        {
+            using (var dbx = new DropboxClient("YOUR ACCESS TOKEN"))
+            {
+                var full = await dbx.Users.GetCurrentAccountAsync();
+                Console.WriteLine("{0} - {1}", full.Name.DisplayName, full.Email);
+            }
+        }
+
+        async Task ListRootFolder(DropboxClient dbx)
+        {
+            var list = await dbx.Files.ListFolderAsync(string.Empty);
+
+            // show folders, then files
+            foreach (var item in list.Entries.Where(i => i.IsFolder))
+            {
+                Console.WriteLine("D  {0}/", item.Name);
+            }
+
+            foreach (var item in list.Entries.Where(i => i.IsFile))
+            {
+                Console.WriteLine("F{0,8} {1}", item.AsFile.Size, item.Name);
+            }
+        }
+
+        async Task Download(DropboxClient dbx, string folder, string file)
+        {
+            using (var response = await dbx.Files.DownloadAsync(folder + "/" + file))
+            {
+                Console.WriteLine(await response.GetContentAsStringAsync());
+            }
+        }
+
+        async Task Upload(DropboxClient dbx, string folder, string file, string content)
+        {
+            using (var mem = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+            {
+                var updated = await dbx.Files.UploadAsync(
+                    folder + "/" + file,
+                    Dropbox.Api.Files.WriteMode.Overwrite.Instance,
+                    body: mem);
+                Console.WriteLine("Saved {0}/{1} rev {2}", folder, file, updated.Rev);
+            }
+        }
+    }
+}
